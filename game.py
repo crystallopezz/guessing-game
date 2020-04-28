6c4346873bde2afbc5f25e44e871da30b730a891
Crystal Lopez <crystallopez289@gmail.com>
2020-04-28 23:55:29 +0000

    check if number in range

--- game.py
+++ game.py
@@ -7,6 +7,8 @@ first_guess = int(input("Can you guess what number I'm thinking of between 1-100
 current_guess = first_guess
 def guessing_game (current_guess, random_number):
     tries = 0
+    if current_guess not in range(1,100):
+        current_guess = int(input("Not in range, please try again"))
     while current_guess != random_number:
         tries = tries+1
         if current_guess > random_number:
@@ -16,4 +18,7 @@ def guessing_game (current_guess, random_number):
     else: 
         tries = tries+1
         print("Congrats, you guessed it in ",tries," tries!")
-guessing_game(current_guess, random_number)
+try:
+    guessing_game(current_guess, random_number)
+except: 
+    current_guess = int(input("Not a valid number, try again"))
