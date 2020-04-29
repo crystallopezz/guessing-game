e3a336b0b5f669c6715c91542165037c57535c96
Crystal Lopez <crystallopez289@gmail.com>
2020-04-29 01:27:24 +0000

    Check if input is integer

--- game.py
+++ game.py
@@ -3,19 +3,20 @@ import random
 print("Hello player!")
 name = input("What is your name?")
 random_number = random.randint(1, 100)
-first_guess = int(input("Can you guess what number I'm thinking of between 1-100?"))
-current_guess = first_guess
-def guessing_game (current_guess, random_number):
-    tries = 0
-    if current_guess < 0 or current_guess > 101:
-        current_guess = int(input("Not in range, please try again"))
-    while current_guess != random_number:
-        tries = tries+1
-        if current_guess > random_number:
-            current_guess = int(input("Too high"))
-        if current_guess < random_number:
-            current_guess = int(input("Too low"))
+tries = 0
+current_guess = input("Can you guess what number I'm thinking of between 1-100?")
+try: 
+    current_guess = int(current_guess)
+except: 
+    current_guess = int(input("Please write a valid number between 1-100"))
+if current_guess < 0 or current_guess > 101:
+    current_guess = int(input("Not in range, please try again"))
+while current_guess != random_number:
+    tries = tries+1
+    if current_guess > random_number:
+        current_guess = int(input("Too high"))
+    if current_guess < random_number:
+        current_guess = int(input("Too low"))
     else: 
         tries = tries+1
         print("Congrats, you guessed it in ",tries," tries!")
-guessing_game (current_guess, random_number)
